3f51d87093249ffe99504a201137b6f95d6f5048
lucaspasti <lucas.pasti@gmail.com>
2025-05-31 16:26:41 -0300

    modified:   app/db/create_table_examples.sql

--- app/db/create_table_examples.sql
+++ app/db/create_table_examples.sql
@@ -1,5 +1,5 @@
 CREATE TABLE examples (
-  id                UUID     DEFAULT gen_random_uuid() PRIMARY KEY,
+  user_id    UUID    NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE PRIMARY KEY,
   competitor_urls   TEXT[],
   favorite_content  TEXT,
   content_to_avoid  TEXT,
